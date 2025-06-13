cd52f6b5c1b6675fb7d1df429106bcf4eebe9e92
joshfpedro <33076791+joshfpedro@users.noreply.github.com>
2025-06-13 14:28:36 -0400

    Fix requirements handling in setup.py and update requirements.txt format

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements(file_path:str) -> List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace('\n', '') for req in requirements if req.strip() and not req.startswith('#')]
+        requirements = [req.replace("\n", "") for req in requirements]
     
     if HYPHEN_E_DOT in requirements:
         requirements.remove(HYPHEN_E_DOT)
