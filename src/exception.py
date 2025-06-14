156217d2a6190079f93c6c6f70f2a59eb4b1074d
joshfpedro <33076791+joshfpedro@users.noreply.github.com>
2025-06-13 20:38:15 -0400

    Fix formatting and add missing newline in exception and logger modules

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 def error_message_details(error, error_detail:sys):
     
@@ -6,11 +7,9 @@ def error_message_details(error, error_detail:sys):
     file_name = exc_tb.tb_frame.f_code.co_filename
     
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
+        file_name, exc_tb.tb_lineno, str(error))
         
-        
-        return error_message
-        )
+    return error_message
     
     
 class CustomException(Exception):
@@ -20,3 +19,4 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
+    
